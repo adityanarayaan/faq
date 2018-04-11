c9f7a7c78861ba5866aa0b77d985160aa328cb12
aditya <ap2289@njit.edu>
2018-04-11 02:25:52 -0400

    created user test.

--- app/User.php
+++ app/User.php
@@ -51,6 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Profile');
     }
-
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
 
 }
