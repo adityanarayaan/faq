0051622cd76205d544941f1da63655b091b98c7b
aditya <ap2289@njit.edu>
2018-04-30 16:43:36 -0400

    Completed all the task assigned in video part 6.

--- app/Answer.php
+++ app/Answer.php
@@ -6,12 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    protected $fillable = ['body'];
+
+
     public function user()
     {
         return $this->belongsTo('App\User');
     }
+
     public function question()
     {
         return $this->belongsTo('App\Question');
-    }//
+    }
 }
